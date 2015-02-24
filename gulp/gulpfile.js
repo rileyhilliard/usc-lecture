e383aad0f4a57b484e1f88195d88aa61d3e0afcf
Riley Hilliard <rhilliard@edgecast.com>
2015-02-23 18:41:35 -0800

    adding node and gulp folders

---
+++ gulp/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+  console.log('Default has been initiated!');
+});
+
+
+gulp.task('something', function() {
+  console.log('Something has been initiated!');
+});
