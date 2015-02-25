601c4b83e85136e1cacc7b42a1a6a82264ef8d87
Riley Hilliard <rhilliard@edgecast.com>
2015-02-24 21:49:04 -0800

    adding angular.js presentation example files

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -3,8 +3,3 @@ var gulp = require('gulp');
 gulp.task('default', function() {
   console.log('Default has been initiated!');
 });
-
-
-gulp.task('something', function() {
-  console.log('Something has been initiated!');
-});
